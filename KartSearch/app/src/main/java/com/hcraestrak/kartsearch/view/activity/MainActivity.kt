2c099c4fcfd4842c1f58cb5e770207e0d458c1c5
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-11-15 17:12:12 +0900

    [EDIT] Change Directory.

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/activity/MainActivity.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hcraestrak.kartsearch
+package com.hcraestrak.kartsearch.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hcraestrak.kartsearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/MainFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/MainFragment.kt
@@ -1,10 +1,11 @@
-package com.hcraestrak.kartsearch
+package com.hcraestrak.kartsearch.view.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hcraestrak.kartsearch.R
 
 class MainFragment : Fragment() {
 
