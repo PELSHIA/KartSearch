49ace77bd2b64b4805b96b3dafcffd03af1adaa5
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-11-15 17:39:06 +0900

    [ADD] Setting Navigation

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/SearchFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/SearchFragment.kt
@@ -5,26 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.hcraestrak.kartsearch.R
-import com.hcraestrak.kartsearch.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
-
-    private lateinit var binding: FragmentMainBinding
+class SearchFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.toSearch.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_searchFragment)
-        }
     }
 }
