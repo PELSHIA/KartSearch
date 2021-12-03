6e04a31e19a02ae947e04e2e9dfb5f80b295e8f5
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-03 20:30:59 +0900

    [EDIT] Set Profile Data Setting

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/InformationFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/InformationFragment.kt
@@ -6,13 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.hcraestrak.kartsearch.R
 import com.hcraestrak.kartsearch.databinding.FragmentInformationBinding
+import com.hcraestrak.kartsearch.model.viewModel.MatchViewModel
+import com.hcraestrak.kartsearch.model.viewModel.UserViewModel
 
 class InformationFragment : Fragment() {
 
     private lateinit var binding: FragmentInformationBinding
+    private val viewModel: MatchViewModel by viewModels()
+    private val args: InformationFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +34,7 @@ class InformationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindingToolbar()
+        searchData()
     }
 
     private fun bindingToolbar() {
@@ -39,4 +49,22 @@ class InformationFragment : Fragment() {
             findNavController().navigate(R.id.action_informationFragment_to_searchFragment)
         }
     }
+
+    private fun searchData() {
+        viewModel.accessIdMatchInquiry(args.accessId)
+        viewModel.getMatchResponseObserver().observe(viewLifecycleOwner, {
+            binding.userNickName.text = it.nickName
+            getUserCharacter("character", it.matches[0].matches[0].character)
+        })
+    }
+
+    private fun getUserCharacter(type: String, id: String) {
+        val storage: FirebaseStorage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        val uri = storageRef.child("$type/$id.png")
+        uri.downloadUrl.addOnSuccessListener {
+            Glide.with(this).load(it).into(binding.userProfileImg)
+        }
+    }
+
 }
