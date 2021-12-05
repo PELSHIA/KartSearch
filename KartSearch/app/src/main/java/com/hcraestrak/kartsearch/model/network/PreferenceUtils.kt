1da31ae46f39e14cd9a1b1a7184b432915d517d5
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-05 14:38:46 +0900

    [EDIT] Token Change

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/model/network/PreferenceUtils.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/model/network/PreferenceUtils.kt
@@ -5,7 +5,7 @@ import android.content.SharedPreferences
 
 object PreferenceUtils {
     private lateinit var preferences: SharedPreferences
-    private const val ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTQwOTgzMTY5NiIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTM5MyIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2Mzc3NDAzNzUsImV4cCI6MTY1MzI5MjM3NSwiaWF0IjoxNjM3NzQwMzc1fQ.jDg_Ro2haGIyyIyZAN7qSOgWQlqwjxTzn2Ffu7OGCT8"
+    private const val ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTQwOTgzMTY5NiIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTM5MyIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2Mzg2ODI2NDMsImV4cCI6MTY1NDIzNDY0MywiaWF0IjoxNjM4NjgyNjQzfQ.3TemvuJHLKX-VnhIJSQ9DuD8zkmRpsYqa-FXyRTMmTQ"
 
     fun init(context: Context) {
         preferences = context.getSharedPreferences("token", Context.MODE_PRIVATE)
