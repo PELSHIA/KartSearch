0baa9b3709f58e0de472cd3565fe2486a07f6427
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-06 10:43:23 +0900

    [EDIT] fix error and add License img

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/InformationFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/InformationFragment.kt
@@ -1,27 +1,35 @@
 package com.hcraestrak.kartsearch.view.fragment
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 import com.hcraestrak.kartsearch.R
 import com.hcraestrak.kartsearch.databinding.FragmentInformationBinding
 import com.hcraestrak.kartsearch.model.viewModel.MatchViewModel
-import com.hcraestrak.kartsearch.model.viewModel.UserViewModel
+import java.io.File
+import java.io.IOException
 
 class InformationFragment : Fragment() {
 
     private lateinit var binding: FragmentInformationBinding
     private val viewModel: MatchViewModel by viewModels()
     private val args: InformationFragmentArgs by navArgs()
+    private lateinit var storageReference: StorageReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,18 +60,52 @@ class InformationFragment : Fragment() {
 
     private fun searchData() {
         viewModel.accessIdMatchInquiry(args.accessId)
-        viewModel.getMatchResponseObserver().observe(viewLifecycleOwner, {
-            binding.userNickName.text = it.nickName
-            getUserCharacter("character", it.matches[0].matches[0].character)
+        viewModel.getMatchResponseObserver().observe(viewLifecycleOwner, { match ->
+            binding.userNickName.text = match.nickName
+            getProfileImage("character", match.matches[0].matches[0].character) // 대표 캐릭터 이미지
+            when(match.matches[0].matches[0].player.rankinggrade2) { // 라이센스 이미지
+                "1" -> getLicenseImage("License", "chobo")
+                "2" -> getLicenseImage("License", "Rookie")
+                "3" -> getLicenseImage("License", "L3")
+                "4" -> getLicenseImage("License", "L2")
+                "5" -> getLicenseImage("License", "L1")
+                "6" -> getLicenseImage("License", "PRO")
+            }
+//            getLicenseImage("License", "PRO")
+
+//            storageReference = FirebaseStorage.getInstance().getReference("UnknownCharacter.png")
         })
     }
 
-    private fun getUserCharacter(type: String, id: String) {
-        val storage: FirebaseStorage = FirebaseStorage.getInstance()
-        val storageRef = storage.reference
-        val uri = storageRef.child("$type/$id.png")
-        uri.downloadUrl.addOnSuccessListener {
-            Glide.with(this).load(it).into(binding.userProfileImg)
+    private fun getProfileImage(type: String, id: String) {
+        storageReference = FirebaseStorage.getInstance().getReference("/$type/$id.png")
+        try {
+            val localFile: File = File.createTempFile("tempfile", ".png")
+            storageReference.getFile(localFile)
+                .addOnSuccessListener {
+                    val bitmap: Bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                    Glide.with(this).load(bitmap).into(binding.userProfileImg)
+                }.addOnFailureListener{
+                    Toast.makeText(requireContext(), "사진 가져오기에 실패했습니다.", Toast.LENGTH_LONG).show()
+                }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getLicenseImage(type: String, id: String) {
+        storageReference = FirebaseStorage.getInstance().getReference("/$type/$id.png")
+        try {
+            val localFile: File = File.createTempFile("tempfile", ".png")
+            storageReference.getFile(localFile)
+                .addOnSuccessListener {
+                    val bitmap: Bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                    Glide.with(this).load(bitmap).into(binding.userLicense)
+                }.addOnFailureListener{
+                    Toast.makeText(requireContext(), "사진 가져오기에 실패했습니다.", Toast.LENGTH_LONG).show()
+                }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
