ab43cb94f20979bf58a4d86c65adc4208bd7f2a9
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-07 15:16:02 +0900

    [ADD] Create UserStatsFragment

---
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/UserStatsFragment.kt
@@ -0,0 +1,26 @@
+package com.hcraestrak.kartsearch.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hcraestrak.kartsearch.R
+import com.hcraestrak.kartsearch.databinding.FragmentUserStatsBinding
+
+class UserStatsFragment : Fragment() {
+
+    private lateinit var binding: FragmentUserStatsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentUserStatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
