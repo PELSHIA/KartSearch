aabc241472e6f200e621294fceb2ec0fc60e1624
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-07 15:16:21 +0900

    [ADD] Create UserInfoFragment

---
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/UserInfoFragment.kt
@@ -0,0 +1,26 @@
+package com.hcraestrak.kartsearch.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hcraestrak.kartsearch.R
+import com.hcraestrak.kartsearch.databinding.FragmentUserInfoBinding
+
+class UserInfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentUserInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentUserInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
