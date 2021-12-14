752e1c9ccca45dde8f7d628537f8fe6df22d97de
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-14 20:59:55 +0900

    [EDIT] binding trackName and KartImg

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/adapter/UserInfoRecyclerViewAdapter.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/adapter/UserInfoRecyclerViewAdapter.kt
@@ -7,10 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.hcraestrak.kartsearch.R
 import com.hcraestrak.kartsearch.view.adapter.data.UserInfoData
+import java.io.File
+import java.io.IOException
 
 class UserInfoRecyclerViewAdapter: RecyclerView.Adapter<UserInfoRecyclerViewAdapter.ViewHolder>() {
 
@@ -28,11 +38,55 @@ class UserInfoRecyclerViewAdapter: RecyclerView.Adapter<UserInfoRecyclerViewAdap
         val map: TextView = view.findViewById(R.id.item_map)
         val time: TextView = view.findViewById(R.id.item_time)
 
-        fun bind(data: UserInfoData, context: Context) {
-            rank.text = "${data.userRank.toString()}/${data.playerCount.toString()}"
-            Glide.with(context).load(data.kart).into(kart)
-            map.text = data.track
-            time.text = data.time.toString()
+        fun bind(data: UserInfoData) {
+            when (data.isRetired) {
+                "0" -> {
+                    rank.text = "${data.userRank}/${data.playerCount}"
+                    time.text = data.time
+                }
+                "1" -> {
+                    rank.text = "Re"
+                    time.text = "-"
+                }
+            }
+            getImage(data.kart)
+            getTrackName(data.track, map)
+        }
+
+        private fun getImage(kartId: String) {
+            val storageReference = FirebaseStorage.getInstance().getReference("/kart/$kartId.png")
+            try {
+                val localFile: File = File.createTempFile("tempfile", ".png")
+                storageReference.getFile(localFile)
+                    .addOnSuccessListener {
+                        val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                        Glide.with(itemView.context).load(bitmap).into(kart)
+                    }.addOnFailureListener{
+                        Toast.makeText(itemView.context, "사진 가져오기에 실패했습니다.", Toast.LENGTH_LONG).show()
+                    }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        private fun getTrackName(trackId: String, view: TextView) {
+            val database: DatabaseReference = Firebase.database.reference
+            database.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (postSnapshot in snapshot.children) {
+                        val id = postSnapshot.child("id").getValue(String::class.java)
+                        val name = postSnapshot.child("name").getValue(String::class.java).toString()
+                        if (id == trackId) {
+                            view.text = name
+                            break
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d("error", "${error.code}: ${error.message}")
+                }
+            })
         }
     }
 
