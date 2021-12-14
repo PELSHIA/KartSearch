b9264ddba02ef22529703e64fca7674cf9daaeb0
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-14 20:55:12 +0900

    [ADD] Create FirebaseViewModel

---
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/model/viewModel/FirebaseViewModel.kt
@@ -0,0 +1,37 @@
+package com.hcraestrak.kartsearch.model.viewModel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class FirebaseViewModel: ViewModel() {
+
+    private lateinit var database: DatabaseReference
+
+    fun getGameType(typeId: String): LiveData<String> {
+        val data = MutableLiveData<String>()
+        database = Firebase.database("https://gametype.firebaseio.com/").reference
+        database.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (postSnapshot in snapshot.children) {
+                    val id = postSnapshot.child("id").getValue(String::class.java)
+                    val name = postSnapshot.child("name").getValue(String::class.java)
+                    if (id == typeId) {
+                        data.postValue(name)
+                        return
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("error", "${error.code}: ${error.message}")
+            }
+        })
+        return data
+    }
+}
+
