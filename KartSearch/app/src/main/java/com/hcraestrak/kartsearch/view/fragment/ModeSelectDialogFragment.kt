b720c35d66bfb829a96466b656676c2bc02c4f6d
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-01-07 00:07:27 +0900

    [FIX] Not Showing RecyclerView Item

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/ModeSelectDialogFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/ModeSelectDialogFragment.kt
@@ -1,19 +1,20 @@
 package com.hcraestrak.kartsearch.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hcraestrak.kartsearch.R
 import com.hcraestrak.kartsearch.databinding.FragmentModeSelectDialogBinding
 import com.hcraestrak.kartsearch.model.viewModel.UserInfoViewModel
 import com.hcraestrak.kartsearch.view.adapter.ModeSelectRecyclerViewAdapter
 import com.hcraestrak.kartsearch.view.decoration.RecyclerViewDecoration
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ModeSelectDialogFragment : DialogFragment() {
 
@@ -36,7 +37,8 @@ class ModeSelectDialogFragment : DialogFragment() {
 
     private fun bindingRecyclerView() {
         val decoration = RecyclerViewDecoration(15)
-        val modeList = resources.getStringArray(R.array.game_mode)
+        val modeArray: Array<String> = resources.getStringArray(R.array.game_mode)
+        var modeList: List<String> = modeArray.toList()
         binding.modeSelectList.apply {
             layoutManager = LinearLayoutManager(context)
             recyclerViewAdapter = ModeSelectRecyclerViewAdapter()
