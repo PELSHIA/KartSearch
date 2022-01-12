7d9a10e27c42322351c99472f355a62a9641011f
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-01-12 20:31:50 +0900

    [EDIT] Edit MatchDetail Response

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/model/network/data/response/MatchDetailPlayer.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/model/network/data/response/MatchDetailPlayer.kt
@@ -7,10 +7,10 @@ data class MatchDetailPlayer(
     val matchType: String,
     val matchResult: String,
     val gameSpeed: Int,
-    val startTime: Date,
-    val endTime: Date,
+    val startTime: String,
+    val endTime: String,
     val playTime: Int,
     val channelName: String,
     val trackId: String,
-    val teams: List<Player>
+    val players: List<Player>
 )
