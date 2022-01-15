a3c3789c40b332a8f521a4f942dd525b8a5f327a
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-01-16 00:45:52 +0900

    [ADD] Add RecentSearch

---
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/adapter/SearchRecyclerViewAdapter.kt
@@ -0,0 +1,62 @@
+package com.hcraestrak.kartsearch.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hcraestrak.kartsearch.R
+import com.hcraestrak.kartsearch.model.db.entity.Search
+
+class SearchRecyclerViewAdapter: RecyclerView.Adapter<SearchRecyclerViewAdapter.ViewHolder>() {
+
+    private val dataSet = mutableListOf<Search>()
+    private var word: String = ""
+    private lateinit var mListener: OnItemClickListener
+
+    fun setOnItemClickListener(listener: (Int) -> Unit) {
+        mListener = object : OnItemClickListener {
+            override fun onClick(id: Int) {
+                listener(id)
+            }
+        }
+    }
+
+    fun getWord() = word
+
+    fun setData(data: List<Search>) {
+        dataSet.clear()
+        dataSet.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val searches: TextView = view.findViewById(R.id.item_searches)
+        val delete: ImageButton = view.findViewById(R.id.item_delete)
+
+        fun bind(data: Search) {
+            searches.text = data.word
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dataSet[position])
+        holder.delete.setOnClickListener {
+            mListener.onClick(1)
+            word = dataSet[position].word
+        }
+
+        holder.searches.setOnClickListener {
+            mListener.onClick(2)
+            word = dataSet[position].word
+        }
+    }
+
+    override fun getItemCount() = dataSet.size
+}
--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/SearchFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/SearchFragment.kt
@@ -8,7 +8,11 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hcraestrak.kartsearch.databinding.FragmentSearchBinding
+import com.hcraestrak.kartsearch.model.db.entity.Search
+import com.hcraestrak.kartsearch.view.adapter.SearchRecyclerViewAdapter
+import com.hcraestrak.kartsearch.viewModel.SearchViewModel
 import com.hcraestrak.kartsearch.viewModel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,7 +20,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var recyclerViewAdapter: SearchRecyclerViewAdapter
     private val viewModel: UserViewModel by viewModels()
+    private val searchViewModel: SearchViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +35,7 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initRecyclerView()
         binding.searchEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 viewModel.getAccessId(binding.searchEditText.text.toString())
@@ -36,6 +43,7 @@ class SearchFragment : Fragment() {
                     if (it == null) {
                         findNavController().navigate(SearchFragmentDirections.actionSearchFragmentToErrorFragment())
                     } else {
+                        setSearchData(it.name)
                         findNavController().navigate(SearchFragmentDirections.actionSearchFragmentToInformationFragment(it.accessId))
                     }
                 })
@@ -43,4 +51,39 @@ class SearchFragment : Fragment() {
             false
         }
     }
+
+    private fun initRecyclerView() {
+        binding.searchRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            recyclerViewAdapter = SearchRecyclerViewAdapter()
+            adapter = recyclerViewAdapter
+        }
+        searchViewModel.getAllWord()
+        searchViewModel.searchWordObserver().observe(viewLifecycleOwner, {
+            recyclerViewAdapter.setData(it)
+        })
+
+        recyclerViewAdapter.setOnItemClickListener {
+            when(it) {
+                1 -> {
+                    deleteSearchData(recyclerViewAdapter.getWord())
+                    recyclerViewAdapter.notifyDataSetChanged()
+                }
+
+            }
+        }
+    }
+
+    private fun setSearchData(word: String) {
+        searchViewModel.insertWord(Search(word))
+    }
+
+    private fun deleteSearchData(word: String) {
+        searchViewModel.deleteWord(Search(word))
+    }
+
+    private fun deleteAllSearchData() {
+        searchViewModel.deleteAllWord()
+    }
+
 }
