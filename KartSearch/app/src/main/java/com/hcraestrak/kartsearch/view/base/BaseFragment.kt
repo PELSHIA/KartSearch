1a49fd49a94f286fdbb41642c180b34392aa1f0d
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-02-12 19:09:31 +0900

    [ADD] BaseFragment

---
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/base/BaseFragment.kt
@@ -0,0 +1,27 @@
+package com.hcraestrak.kartsearch.view.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+abstract class BaseFragment<VB: ViewDataBinding, VM: ViewModel>(@LayoutRes val layoutRes: Int): Fragment() {
+
+    protected abstract val viewModel: VM
+    protected lateinit var binding: VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
+}
