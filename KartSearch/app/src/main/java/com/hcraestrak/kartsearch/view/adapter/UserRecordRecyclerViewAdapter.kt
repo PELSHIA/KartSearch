c66f5c973c09ee27cec932d13b31958d84339290
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-02-25 01:55:45 +0900

    [EDIT] UserRecordRecyclerViewAdapter to LoadMore

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/adapter/UserRecordRecyclerViewAdapter.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/adapter/UserRecordRecyclerViewAdapter.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.hcraestrak.kartsearch.R
+import com.hcraestrak.kartsearch.databinding.ItemLoadMoreBinding
 import com.hcraestrak.kartsearch.databinding.ItemRecordBinding
 import com.hcraestrak.kartsearch.view.adapter.data.UserInfoData
 import com.hcraestrak.kartsearch.view.adapter.listener.OnItemClickListener
 
-class UserRecordRecyclerViewAdapter: RecyclerView.Adapter<UserRecordRecyclerViewAdapter.ViewHolder>() {
+class UserRecordRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val VIEW_TYPE_ITEM = 0
+    private val VIEW_TYPE_LOAD = 1
 
     private val data = mutableListOf<UserInfoData>()
     private var matchId: String = ""
@@ -25,13 +29,22 @@ class UserRecordRecyclerViewAdapter: RecyclerView.Adapter<UserRecordRecyclerView
     }
 
     fun setData(data: List<UserInfoData>) {
-        this.data.clear()
         this.data.addAll(data)
+        this.data.add(UserInfoData(0, " ", " ", " ", " ", " ", " ", " "))
+        notifyDataSetChanged()
+    }
+
+    fun clearData() {
+        this.data.clear()
         notifyDataSetChanged()
     }
 
     fun getMatchId() = matchId
     fun getIsWin() = isWin
+
+    class LoadMoreViewHolder(val binding: ItemLoadMoreBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
 
     class ViewHolder(val binding: ItemRecordBinding) : RecyclerView.ViewHolder(binding.root) {
 
@@ -61,27 +74,45 @@ class UserRecordRecyclerViewAdapter: RecyclerView.Adapter<UserRecordRecyclerView
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemRecordBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_ITEM -> {
+                val binding = ItemRecordBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                ViewHolder(binding)
+            }
+            else -> {
+                val binding = ItemLoadMoreBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                LoadMoreViewHolder(binding)
+            }
+        }
+
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(data[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewHolder) {
+            holder.bind(data[position])
 
-        holder.itemView.setOnClickListener {
-            matchId = data[position].matchId
-            isWin = if (data[position].isWin == "") {
-                0
-            } else {
-                data[position].isWin.toInt()
+            holder.itemView.setOnClickListener {
+                matchId = data[position].matchId
+                isWin = if (data[position].isWin == "") {
+                    0
+                } else {
+                    data[position].isWin.toInt()
+                }
+                mListener.onClick(1)
             }
-            mListener.onClick(1)
+        } else if (holder is LoadMoreViewHolder) {
+            holder.binding.loadMoreLayout.setOnClickListener {
+                mListener.onClick(2)
+            }
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return position
+        return when (data[position].matchId) {
+            " " -> VIEW_TYPE_LOAD
+            else -> VIEW_TYPE_ITEM
+        }
     }
 
     override fun getItemCount() = data.size
