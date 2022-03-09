9eed7562ed62710e971fcb88cb00286d4a6bdb8e
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-03-09 22:03:30 +0900

    [ADD] Add BookMarkRecyclerview

---
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/adapter/BookmarkRecyclerViewAdapter.kt
@@ -0,0 +1,64 @@
+package com.hcraestrak.kartsearch.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hcraestrak.kartsearch.R
+import com.hcraestrak.kartsearch.databinding.ItemBookmarkBinding
+import com.hcraestrak.kartsearch.databinding.ItemSearchBinding
+import com.hcraestrak.kartsearch.model.db.entity.Bookmark
+import com.hcraestrak.kartsearch.model.db.entity.Search
+import com.hcraestrak.kartsearch.view.adapter.listener.OnItemClickListener
+
+class BookmarkRecyclerViewAdapter: RecyclerView.Adapter<BookmarkRecyclerViewAdapter.ViewHolder>() {
+
+    private val dataSet = mutableListOf<Bookmark>()
+    private var word: String = ""
+    private lateinit var mListener: OnItemClickListener
+
+    fun setOnItemClickListener(listener: (Int) -> Unit) {
+        mListener = object : OnItemClickListener {
+            override fun onClick(id: Int) {
+                listener(id)
+            }
+        }
+    }
+
+    fun getWord() = word
+
+    fun setData(data: List<Bookmark>) {
+        dataSet.clear()
+        dataSet.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(val binding: ItemBookmarkBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Bookmark) {
+            binding.bookmark = data
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemBookmarkBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dataSet[position])
+
+        holder.binding.itemDeleteBookmark.setOnClickListener {
+            word = dataSet[position].nickName
+            mListener.onClick(1)
+        }
+
+        holder.binding.itemBookmark.setOnClickListener {
+            word = dataSet[position].nickName
+            mListener.onClick(2)
+        }
+    }
+
+    override fun getItemCount() = dataSet.size
+}
--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/SearchFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/fragment/SearchFragment.kt
@@ -1,20 +1,26 @@
 package com.hcraestrak.kartsearch.view.fragment
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hcraestrak.kartsearch.R
 import com.hcraestrak.kartsearch.databinding.FragmentSearchBinding
+import com.hcraestrak.kartsearch.model.db.entity.Bookmark
 import com.hcraestrak.kartsearch.model.db.entity.Search
 import com.hcraestrak.kartsearch.model.network.data.response.UserInfo
+import com.hcraestrak.kartsearch.view.adapter.BookmarkRecyclerViewAdapter
 import com.hcraestrak.kartsearch.view.adapter.SearchRecyclerViewAdapter
 import com.hcraestrak.kartsearch.view.base.BaseFragment
+import com.hcraestrak.kartsearch.viewModel.BookmarkViewModel
 import com.hcraestrak.kartsearch.viewModel.SearchViewModel
 import com.hcraestrak.kartsearch.viewModel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,8 +29,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(R.layout.fragment_search) {
 
     private lateinit var recyclerViewAdapter: SearchRecyclerViewAdapter
+    private lateinit var bookmarkRecyclerViewAdapter: BookmarkRecyclerViewAdapter
     private val userViewModel: UserViewModel by viewModels()
     override val viewModel: SearchViewModel by viewModels()
+    private val bookmarkViewModel: BookmarkViewModel by viewModels()
+    private var mode: Boolean = true // true 최근검색어 false 즐겨찾기
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +48,8 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(R.la
         viewModel.getAllWord()
         initRecyclerView()
         search()
+        changeMode()
+        allDelete()
     }
 
     private fun search() {
@@ -76,11 +87,12 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(R.la
             recyclerViewAdapter = SearchRecyclerViewAdapter()
             adapter = recyclerViewAdapter
         }
-        viewModel.getAllWord()
-        viewModel.searchWord.observe(viewLifecycleOwner, {
-            val list = it.reversed()
-            recyclerViewAdapter.setData(list)
-        })
+        binding.bookmarkRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            bookmarkRecyclerViewAdapter = BookmarkRecyclerViewAdapter()
+            adapter = bookmarkRecyclerViewAdapter
+        }
+        setData()
         recyclerViewAdapter.setOnItemClickListener {
             val word: String = recyclerViewAdapter.getWord()
             when(it) {
@@ -91,6 +103,76 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(R.la
                 2 -> {
                     getSearch(word)
                 }
+            }
+        }
+
+        bookmarkRecyclerViewAdapter.setOnItemClickListener {
+            val nickName: String = bookmarkRecyclerViewAdapter.getWord()
+            when (it) {
+                1 -> {
+                    bookmarkViewModel.deleteNickName(Bookmark(nickName))
+                    bookmarkRecyclerViewAdapter.notifyDataSetChanged()
+                }
+                2 -> {
+                    getSearch(nickName)
+                }
+            }
+        }
+    }
+
+    private fun setData() {
+        viewModel.getAllWord()
+        viewModel.searchWord.observe(viewLifecycleOwner, {
+            val list = it.reversed()
+            recyclerViewAdapter.setData(list)
+        })
+
+        bookmarkViewModel.getAllNickName()
+        bookmarkViewModel.bookmark.observe(viewLifecycleOwner, {
+            val list = it.reversed()
+            bookmarkRecyclerViewAdapter.setData(list)
+        })
+    }
+
+    private fun changeMode() {
+        if (mode) {
+            bookMarkMode()
+        } else {
+            recentSearch()
+        }
+        
+        binding.bookmark.setOnClickListener {
+            mode = false
+            bookMarkMode()
+        }
+        binding.recentSearch.setOnClickListener {
+            mode = true
+            recentSearch()
+        }
+    }
+
+    private fun bookMarkMode() {
+        binding.searchRecyclerView.visibility = View.GONE
+        binding.bookmarkRecyclerView.visibility = View.VISIBLE
+        binding.bookmark.setTextColor(ContextCompat.getColor(binding.bookmark.context, R.color.light_blue))
+        binding.recentSearch.setTextColor(ContextCompat.getColor(binding.recentSearch.context, R.color.black))
+    }
+
+    private fun recentSearch() {
+        binding.searchRecyclerView.visibility = View.VISIBLE
+        binding.bookmarkRecyclerView.visibility = View.GONE
+        binding.bookmark.setTextColor(ContextCompat.getColor(binding.bookmark.context, R.color.black))
+        binding.recentSearch.setTextColor(ContextCompat.getColor(binding.recentSearch.context, R.color.light_blue))
+    }
+
+    private fun allDelete() {
+        binding.allDelete.setOnClickListener {
+            if (mode) {
+                viewModel.deleteAllWord()
+                recyclerViewAdapter.notifyDataSetChanged()
+            } else {
+                bookmarkViewModel.deleteAllNickName()
+                bookmarkRecyclerViewAdapter.notifyDataSetChanged()
             }
         }
     }
