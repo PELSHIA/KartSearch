7c46c4b6f33a3156f6046407e476e6f5d6fbe9a5
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-09-25 14:42:25 +0900

    [FIX] Api Key Store

--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/model/network/PreferenceUtils.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/model/network/PreferenceUtils.kt
@@ -2,10 +2,11 @@ package com.hcraestrak.kartsearch.model.network
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.hcraestrak.kartsearch.BuildConfig
 
 object PreferenceUtils {
     private lateinit var preferences: SharedPreferences
-    private const val ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTQwOTgzMTY5NiIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTM5MyIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2Mzg2ODI2NDMsImV4cCI6MTY1NDIzNDY0MywiaWF0IjoxNjM4NjgyNjQzfQ.3TemvuJHLKX-VnhIJSQ9DuD8zkmRpsYqa-FXyRTMmTQ"
+    private const val ACCESS_TOKEN = BuildConfig.API_KEY
 
     fun init(context: Context) {
         preferences = context.getSharedPreferences("token", Context.MODE_PRIVATE)
--- KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/base/BaseFragment.kt
+++ KartSearch/app/src/main/java/com/hcraestrak/kartsearch/view/base/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.hcraestrak.kartsearch.view.base
 
+import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,6 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import com.hcraestrak.kartsearch.BR
 import com.hcraestrak.kartsearch.viewModel.ModeViewModel
